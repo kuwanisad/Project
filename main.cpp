0176bf9a2a4ea5fb3c6a8bdd02aa2f0e09274115
kuwanisad <rlawhgn12345@gmail.com>
2025-09-22 09:08:19 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+#include<vector>
+#include<windows.h>
+#include<unordered_map>
+#include<string.h>
+#include<unordered_set>
+#include<random>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	int num = 0;
+	for (int i = 1; i <= 100; i++)
+		num += i;
+
+	cout << num << endl;
+
+	return 0;
+}
