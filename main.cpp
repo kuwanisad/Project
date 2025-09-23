b5a60450a3a240b9473591d1f85f1ca405774881
kuwanisad <rlawhgn12345@gmail.com>
2025-09-23 09:18:09 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,16 +12,33 @@ using namespace std;
 
 int main()
 {
-	int StarLevel = 5;
+    std::random_device rd;                          // 시드 생성기
+    std::mt19937 gen(rd());                         // Mersenne Twister 엔진
+    std::uniform_int_distribution<> dist(0, 51);   // 1~100 사이의 균등 분포
 
-	for (int i = StarLevel; i > 0; --i)
-	{
-		for (int j = 0; j < StarLevel - i; ++j)
-			cout << " ";
-		for (int j = 0; j < i; ++j)
-			cout << "*";
-		cout << endl;
-	}
+    int random_number = dist(gen);
+
+    vector<int> arr(52, 0);
+
+    for (int i = arr.size() - 1; i >= 0; --i)
+    {
+        arr[i] = i + 1;
+    }
+
+    for (int i = 0; i < arr.size() * 0.5f; i++)
+    {
+        int index1 = dist(gen);
+        int index2 = dist(gen);
+
+        int swap_num = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = swap_num;
+    }
+
+    for (int i = arr.size() - 1; i >= 0; --i)
+    {
+        cout << arr[i] << endl;
+    }
 
 	return 0;
 }
